ee48aa4f64003c6f5580e28f56fb67ec65f28cf6
YasinZaki <55282147+YasinZaki@users.noreply.github.com>
2020-03-02 13:48:20 +0000

    Add files via upload

---
+++ Area-Calculator-master/Area Calculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U16_Modular_Calculator
+{
+    class Calculator
+    {
+         //Rectangle Area Function
+        public double Rectangle(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        
+        //Circle Area Function
+        public double Circle(double radius)
+        {
+            
+                
+                double pi = Math.PI;
+
+
+            return pi * (radius * radius); ;
+
+         }
+
+        //Triangle Area Function 
+        public double Triangle(double num1, double num2)
+        {
+            return num1 * num2 / 2;
+        }
+
+ 
+
+   
+    }
+}
